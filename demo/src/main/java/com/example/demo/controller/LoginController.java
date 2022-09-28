123b553b4b50691b915f0385e9f6ee4d7c9edd8c
Nida903 <nidaparveen.903@gmail.com>
2022-09-29 01:14:11 +0530

    updated post api

--- demo/src/main/java/com/example/demo/controller/LoginController.java
+++ demo/src/main/java/com/example/demo/controller/LoginController.java
@@ -2,16 +2,18 @@ package com.example.demo.controller;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.pojo.User;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/myshopping")
+@CrossOrigin
 public class LoginController {
 
 	/*
@@ -29,22 +31,10 @@ public class LoginController {
 	}
 
 	@PostMapping(path = "/createUser")
-	public ResponseEntity<User> userLogin(@RequestParam String userName, @RequestParam String password) {
+	public ResponseEntity<User> userLogin(@RequestBody User user) {
 		HttpHeaders responseHeaders = new HttpHeaders();
-		responseHeaders.set("Content-Type", "application/json; charset=utf8");
-		
-		responseHeaders.set("Access-Control-Allow-Origin", "*");
-		responseHeaders.set("Access-Control-Allow-Credentials", "true");
-		responseHeaders.set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-		responseHeaders.set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-		
-		System.out.println(userName); // return ResponseEntity.ok();
-		return ResponseEntity.ok().headers(responseHeaders).body(new User(1, "vivek", "pass"));
-		/*
-		 * if (userName.equals("nida")) { System.out.println("Valid User"); } else {
-		 * System.out.println("Invalid User"); }
-		 */
-
+		responseHeaders.set("Content-Type", "application/json");
+		responseHeaders.set("Accept", "application/json");
+		return ResponseEntity.ok().headers(responseHeaders).body(user);
 	}
-	 
 }
